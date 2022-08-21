7d3deb1eeb58e9d578aa39d33c048b0037b0b8d7
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-21 19:47:09 +0530

    Third Activity Created

---
+++ app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/ThirdActivity_Matric_Input.java
@@ -0,0 +1,13 @@
+package com.example.mad_t2_and_t3_bmi_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThirdActivity_Matric_Input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_matric_input);
+    }
+}
