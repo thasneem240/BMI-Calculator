340bfef7231acab3319902b68161c8d7b384b960
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-21 21:44:36 +0530

    Third Activity Nearly Completed have to do slider

--- app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/SecondActivity.java
+++ app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/SecondActivity.java
@@ -4,21 +4,55 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity
 {
+    private Button buttonMetric;
+    private Button buttonImperial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        buttonMetric = findViewById(R.id.buttonMetric);
+        buttonImperial = findViewById(R.id.buttonImperial);
+
+
+        buttonMetric.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity("Metric");
+            }
+        });
+
+        buttonImperial.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity("Imperial");
+            }
+        });
     }
 
     public static Intent getIntent(Context context)
     {
-        Intent intent = new Intent(context,SecondActivity.class);
-
-        return intent;
+        return new Intent(context,SecondActivity.class);
     }
+
+    /* Start the Third Activity  Activity And pass the Data between them */
+
+    public void startActivity(String choice)
+    {
+        Intent intent = ThirdActivity_Get_Input.getIntent(SecondActivity.this,choice);
+
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/ThirdActivity_Get_Input.java
+++ app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/ThirdActivity_Get_Input.java
@@ -1,13 +1,62 @@
 package com.example.mad_t2_and_t3_bmi_calculator;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class ThirdActivity_Get_Input extends AppCompatActivity {
+public class ThirdActivity_Get_Input extends AppCompatActivity
+{
+    private static final String CHOISE = "UserChoise";
+    private String choice = "";
+    private TextView textTitle;
+    private TextView textViewWeight;
+    private TextView textViewHeight;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_matric_input);
+
+        textTitle = findViewById(R.id.textTitle);
+        textViewWeight = findViewById(R.id.textViewWeight);
+        textViewHeight = findViewById(R.id.textViewHeight);
+
+        Intent intent = getIntent(); // Get the Intent
+
+        choice = intent.getStringExtra(CHOISE); // get the data from second Activity
+
+        /* Set the User interface based on the user choice */
+
+        setuserInterface(textTitle,textViewWeight,textViewHeight);
+    }
+
+
+    public static Intent getIntent(Context context, String choice)
+    {
+        Intent intent = new Intent(context,ThirdActivity_Get_Input.class);
+        intent.putExtra(CHOISE,choice);
+
+        return intent;
+    }
+
+    private void setuserInterface(TextView title, TextView weight, TextView height)
+    {
+        if(choice.equals("Metric"))
+        {
+            title.setText("You have selected Metric system");
+            weight.setText("KG");
+            height.setText("CM");
+        }
+        else
+        {
+            /* Imperial */
+
+            title.setText("You have selected Imperial system");
+            weight.setText("LB");
+            height.setText("Inch");
+        }
     }
 }
