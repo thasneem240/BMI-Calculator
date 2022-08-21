71d9946ff39d5486efb8aab3008808eed3dc8650
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-21 19:03:46 +0530

    displayPopUpMessage Completed

--- app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/MainActivity.java
+++ app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/MainActivity.java
@@ -1,18 +1,48 @@
 package com.example.mad_t2_and_t3_bmi_calculator;
 
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button buttonYes;
+    private Button buttonNo;
+    private CoordinatorLayout myCoordinatorLayot;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonYes = (Button) findViewById(R.id.buttonYes);
+        buttonNo = (Button) findViewById(R.id.buttonNo);
+        myCoordinatorLayot = findViewById(R.id.myCoordinatorLayout);
+
+        buttonNo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                displayPopUpMessage();
+            }
+        });
+
     }
 
+    private void displayPopUpMessage()
+    {
+        String message  = "The User Age Must be more than 20 In order to   Calculate the BMI ";
+        Snackbar snackbar = Snackbar.make(myCoordinatorLayot,message,Snackbar.LENGTH_LONG);
 
+        snackbar.show();
+    }
 
 
 }
