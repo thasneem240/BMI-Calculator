d686073e3a8792dc8c1356f823c1b127a4281f01
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-21 09:19:27 +0530

    Empty Project

---
+++ app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mad_t2_and_t3_bmi_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
