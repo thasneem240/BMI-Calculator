4e5f52ef8f91ffe50acc8cc1f392f27bb78985ae
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-22 16:08:19 +0530

    SLider and Exception Part completed

--- app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/ThirdActivity_Get_Input.java
+++ app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/ThirdActivity_Get_Input.java
@@ -4,10 +4,17 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.slider.Slider;
+import com.google.android.material.snackbar.Snackbar;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class ThirdActivity_Get_Input extends AppCompatActivity
@@ -20,6 +27,9 @@ public class ThirdActivity_Get_Input extends AppCompatActivity
     private Button buttonNext;
     private EditText textWeight;
     private EditText textHeight;
+    private Slider sliderWeight;
+    private Slider sliderHeight;
+    private ConstraintLayout myConstraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,6 +43,9 @@ public class ThirdActivity_Get_Input extends AppCompatActivity
         buttonNext = findViewById(R.id.buttonNext);
         textWeight = findViewById(R.id.textWeight);
         textHeight = findViewById(R.id.textHeight);
+        sliderWeight = findViewById(R.id.sliderWeight);
+        sliderHeight = findViewById(R.id.sliderHeight);
+        myConstraintLayout = findViewById(R.id.myConstraintLayout);
 
 
         Intent intent = getIntent(); // Get the Intent
@@ -44,15 +57,82 @@ public class ThirdActivity_Get_Input extends AppCompatActivity
         setUserInterface();
 
         buttonNext.setOnClickListener(view -> {
-            /* Get the input from User */
-            double weight = Double.parseDouble(textWeight.getText().toString());
-            double height = Double.parseDouble(textHeight.getText().toString());
 
-            Intent intent1 = FourthActivity_FinalResult.getIntent(ThirdActivity_Get_Input.this,
-                    weight,height,choice);
+            /* Check the Field is Empty or not */
+            if(!checkEmpty(textWeight) && !checkEmpty(textHeight))
+            {
+                    /* Get the input from User */
+                double weight = Double.parseDouble(textWeight.getText().toString());
+                double height = Double.parseDouble(textHeight.getText().toString());
 
-            startActivity(intent1);
+                /* Check the input whether it is valid or not */
+                if(!checkValue(weight,height))
+                {
+                    Intent intent1 = FourthActivity_FinalResult.getIntent(ThirdActivity_Get_Input.this,
+                            weight,height,choice);
+
+                    startActivity(intent1);
+                }
+
+            }
+
+
         });
+
+        sliderWeight.addOnSliderTouchListener(touchListener1);
+        sliderHeight.addOnSliderTouchListener(touchListener2);
+
+        textWeight.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                double newValue = Double.parseDouble(textWeight.getText().toString());
+                if( newValue <= 0 && newValue <= 300)
+                {
+                    sliderWeight.setValue((float)newValue);
+                }
+            }
+        });
+
+        textHeight.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+
+                double newValue = Double.parseDouble(textHeight.getText().toString());
+                if( newValue <= 0 && newValue <= 300)
+                {
+                    sliderHeight.setValue((float)newValue);
+                }
+            }
+        });
+
     }
 
 
@@ -85,4 +165,105 @@ public class ThirdActivity_Get_Input extends AppCompatActivity
             textViewHeight.setText("Inch");
         }
     }
+
+    private final Slider.OnSliderTouchListener touchListener1 =
+            new Slider.OnSliderTouchListener() {
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onStartTrackingTouch(Slider slider)
+                {
+
+                }
+
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onStopTrackingTouch(Slider slider)
+                {
+                    double value = slider.getValue();
+                    String str = String.format("%.2f",value);
+
+                    textWeight.setText(str);
+                }
+            };
+
+
+    private final Slider.OnSliderTouchListener touchListener2 =
+            new Slider.OnSliderTouchListener() {
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onStartTrackingTouch(Slider slider)
+                {
+
+                }
+
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onStopTrackingTouch(Slider slider)
+                {
+                    double value = slider.getValue();
+                    String str = String.format("%.2f",value);
+
+                    textHeight.setText(str);
+                }
+            };
+
+
+    /* Validate Edit Text field is Empty or not */
+
+    public boolean checkEmpty(EditText editText)
+    {
+        boolean isEmpty = false;
+
+        if(editText.length() == 0)
+        {
+            isEmpty = true;
+            editText.setError("Empty field!! Please Enter the input or select through slider ");
+        }
+
+        return isEmpty;
+    }
+
+
+    public boolean checkValue(double pWeight, double pHeight)
+    {
+        boolean invalidValue = false;
+
+        if( pWeight <= 0 || pHeight <= 0)
+        {
+            invalidValue = true;
+            String errorMessage = " Please input Positive Numbers as input";
+            displayPopUpMessage(errorMessage);
+            clearFields();
+        }
+        else
+        {
+            if( pWeight > 300 || pHeight > 300)
+            {
+                invalidValue = true;
+                String errorMessage = " Please input values between 0-300";
+                displayPopUpMessage(errorMessage);
+                clearFields();
+            }
+        }
+
+        return  invalidValue;
+    }
+
+
+    private void displayPopUpMessage(String errorMessage)
+    {
+        Snackbar snackbar = Snackbar.make(myConstraintLayout,errorMessage,Snackbar.LENGTH_LONG);
+
+        snackbar.show();
+    }
+
+    private void clearFields()
+    {
+        textWeight.setText("");
+        sliderWeight.setValue(0);
+
+        textHeight.setText("");
+        sliderHeight.setValue(0);
+    }
+
 }
