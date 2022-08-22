a3ccd2a6a430190a0fff10c34c500c9d9c56eb6b
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-22 11:45:12 +0530

    Set the color and Messgae for Ui4

--- app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/FourthActivity_FinalResult.java
+++ app/src/main/java/com/example/mad_t2_and_t3_bmi_calculator/FourthActivity_FinalResult.java
@@ -1,10 +1,13 @@
 package com.example.mad_t2_and_t3_bmi_calculator;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
 
 public class FourthActivity_FinalResult extends AppCompatActivity
 {
@@ -57,11 +60,13 @@ public class FourthActivity_FinalResult extends AppCompatActivity
             return intent;
     }
 
+    @SuppressWarnings("ConstantConditions")
+    @SuppressLint("DefaultLocale")
     private void setUserInterface()
     {
         String weightUnit = "KG";
         String heightUnit = "CM";
-        String str = "HI";
+        String str;
 
         if(!choice.equals("Metric"))
         {
@@ -76,16 +81,25 @@ public class FourthActivity_FinalResult extends AppCompatActivity
         str = String.format("Your height is %.3f %s",height,heightUnit);
         inputHeight.setText(str);
 
-        double result = calculateBMI();
-        str = String.format("%.3f",result);
+        double result = calculateBMI(); // Get the BMI result
 
+        str = String.format("%.3f",result);
         resultBox.setText(str);
+
+       /* Get the Message and Background color from pair Object */
+        Pair<String, Integer> messageAndColor = getMessageAndBacColor(result);
+
+        String message = String.valueOf(messageAndColor.first);
+         int color = (int)messageAndColor.second;
+
+        resultBoxMessage.setText(message);
+        resultBoxMessage.setBackgroundColor(color);
     }
 
 
     private double calculateBMI()
     {
-        double bmi = 0.0;
+        double bmi;
 
         if(choice.equals("Metric"))
         {
@@ -96,10 +110,64 @@ public class FourthActivity_FinalResult extends AppCompatActivity
             bmi = ( weight / Math.pow(height,2) ) * 703;
         }
 
-        /* Round up to 3 decimal Places and return the BMI*/
-
-       // return Math.round(bmi*1000)/1000;
         return bmi;
+    }
+
+
+    /* Return Multiple values Through pair Object class */
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    private Pair <String,Integer> getMessageAndBacColor(double bmi)
+    {
+        String message;
+        int color;
+
+
+        if(bmi < 18.5)
+        {
+            message = "Underweight";
+            color = getResources().getColor(android.R.color.holo_purple);
+        }
+        else
+        {
+            if(bmi <= 24.9)
+            {
+                message = "Healthy Weight";
+                color = getResources().getColor(R.color.Healthyweight);
+            }
+            else
+            {
+                if(bmi <= 29.9)
+                {
+                    message = "Overweight but not obese";
+                    color = getResources().getColor(R.color.Overweight);
+                }
+                else
+                {
+                    if(bmi <= 34.9)
+                    {
+                        message = "Obese class I";
+                        color = getResources().getColor(R.color.ObeseI);
+                    }
+                    else
+                    {
+                        if(bmi <= 39.9)
+                        {
+                            message = "Obese class II";
+                            color = getResources().getColor(R.color.ObeseII);
+                        }
+                        else
+                        {
+                            message = "Obese class III";
+                            color = getResources().getColor(R.color.ObeseIII);
+                        }
+                    }
+                }
+            }
+        }
+
+        Pair <String,Integer> pair = new Pair<>(message,color);
+
+        return pair;
     }
 
 }
